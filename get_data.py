e60b0726b63e4c13fc9d7eeea438554164f76aae
3p1cPh4r40h <russaz09@hotmail.com>
2023-02-06 11:23:24 -0500

    initial data collection
    get_data.py generates a card_df.pkl file containing all the data we need from the Pokemon cards.

---
+++ get_data.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from pokemontcgsdk import Card
+
+cards = Card.where(q='supertype:Pokémon')
+
+cards_array = []
+for card in cards:
+    card_info = {
+        "name": card.name,
+        "subtypes": card.subtypes,
+        "rules": card.rules,
+        "types": card.types,
+        "hp": card.hp,
+        "attacks": card.attacks,
+        "weaknesses": card.weaknesses,
+        "convertedRetreatCost": card.convertedRetreatCost,
+        "legalities": card.legalities,
+        "evolvesFrom": card.evolvesFrom
+    }
+    cards_array.append(card_info)
+print(cards_array.__len__)
+card_df = pd.DataFrame(cards_array)
+pd.to_pickle(card_df, 'card_df.pkl')
+print(card_df.head())
