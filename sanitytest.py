e9f4e595c15dd6dda6c1fca5302bf312b40343e4
percypie <allertonnicholas9@gmail.com>
2023-02-17 20:12:56 -0500

    More work on sanity testing
    commented out most of it when I was done with it, will go back later and comment and organize better

--- sanitytest.py
+++ sanitytest.py
@@ -1,11 +1,16 @@
+#%%
 import pandas as pd
 import matplotlib.pyplot as plt
-card_df = pd.read_pickle('card_df.pkl')
-sample = card_df.sample(1000)
+card_df = pd.read_pickle('data/card_df.pkl')
+sample = card_df.sample(100)
 #print(sample)
 #sample.to_clipboard()
 #print(sample.isnull().sum())
 #sample.isnull().sum().to_clipboard()
 #dupcheck = .astype(str).duplicated()
 #print(dupcheck.sum())
-sample.types.value_counts().plot(kind='bar')
+#card_df.name.value_counts().nlargest(10).plot(kind='bar',xlabel='Name',ylabel='Count',title='Most Common Names')
+#sample.name.value_counts().plot(kind='bar',xlabel='Name',ylabel='Count',title='Amount of Each Type')
+print(sample.groupby('types'))
+#sample.astype(str).loc[].name.value_counts().nlargest(10).plot(kind='bar',xlabel='Name',ylabel='Count',title='Amount of Each Type')
+# %%
