113552813d177bf48444e628f7ebd42593c235dc
percypie <allertonnicholas9@gmail.com>
2023-03-19 03:13:24 -0400

    Update recurrentgen.py
    Worked more on the recurrent neural network, having errors with the inputs into the model. still need to fix

--- recurrentgen.py
+++ recurrentgen.py
@@ -1,6 +1,8 @@
+from tensorflow import keras
 from keras.models import Sequential
-from keras.models import LSTM, Dense, Masking, Dropout
+from keras.layers import LSTM, Dense, Masking, Dropout
 from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
 import pickle
 import pandas as pd
 import numpy as np
@@ -11,7 +13,7 @@ import matplotlib.pyplot as plt
 #-----------------------------------
 
 #getting the data from the pickle file
-with open("card_df.pkl", "") as k:
+with open("data/card_df.pkl","rb") as k:
     df = pickle.load(k)
 
 #converting the data from strings into numeric values
@@ -20,20 +22,38 @@ df['name'] = enc.fit_transform(df['name'])
 
 #standardizing the integers
 sca = StandardScaler()
-df['hp'] = sca.fit_transform(df['hp'])
+#df['hp'] = sca.fit_transform(df['hp'])
 
-#preparing the test and train sets
-trainsize = int(len(df)*0.7)
-traindata = df.iloc[:trainsize]
-testdata =  df.iloc[trainsize:]
+#preparing the test and train sets with the test set bing 30% of the data
+traindata, testdata = train_test_split(df, test_size=0.3, random_state=42)
 
 #-----------------------------------
 #          Model Creation
 #-----------------------------------
+
 model = Sequential()
 
 model.add(LSTM(64,input_shape=(None, 10)))
 model.add(Dense(1,activation='sigmoid'))
 
-#todo
-model.compile()
+model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+
+#-----------------------------------
+#           Training
+#-----------------------------------
+
+def prepdata(data):
+    x, y = [], []
+    for i in range(len(data)-1):
+        x.append(data.iloc[i:i+1,1:].values)
+        y.append(data.iloc[i+1,0])
+    return np.asarray(x), np.asarray(y)
+
+train_x, train_y = prepdata(traindata)
+
+model.fit(train_x,train_y,epochs=50,batch_size=32)
+
+test_x, test_y = prepdata(testdata)
+
+loss, acc = model.evaluate(test_x,test_y)
+print("Accuracy: ", acc)
